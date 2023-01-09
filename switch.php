9f28e5de6d9b0ce303283eeadae732bd2e3935c8
francescocanini02 <francesco.canini@enaipriminifad.org>
2023-01-09 11:37:50 +0100

    switch

--- switch.php
+++ switch.php
@@ -11,13 +11,28 @@
 <body>
     <?php
     //quanti giorni mancano alla fine del mese
+    echo date("d/m/Y");
+    $m = date("m");
+switch($m){
+        case 2:
+            $giorni_del_mese = 28;
+            break;
+            case 4;
+            case 6;
+            case 9;
+            case 11;
+            $giorni_del_mese = 30;
+            break;
+            default: 
+            $giorni_del_mese = 31;
+}
     
-    echo date("m");
+    $giorni_mancanti = $giorni_del_mese - date("d");
+    echo "giorni mancanti = $giorni_mancanti";
     
-    
-    
-    
-    
+    //per casa
+    //quanti giorni mancano al giorno del tuo compleanno?
+    // prima valuto ilse poi i mesi di differenza poi giorni del mese
     
     
     
